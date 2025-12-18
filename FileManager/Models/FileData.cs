4b24311fadf4ee1e25cef7064490bfc24abeb495
mxgl <78367639+MaxGorel@users.noreply.github.com>
2025-12-18 10:52:05 +0300

    Task2 fix
    Исправление отображения даты и типа для "..."

--- FileManager/Models/FileData.cs
+++ FileManager/Models/FileData.cs
@@ -5,11 +5,11 @@ namespace WpfTest.Models
     class FileData
     {
         public required string Name { get; set; }
-        public DateTime ChangedTime { get; set; }
-        public required string Type { get; set; }
+        public DateTime? ChangedTime { get; set; }
+        public string? Type { get; set; }
         public string? Size { get; set; }
 
 
-        public readonly static FileData LinkToBackFileData = new() { Name = "...", Type = "...", Size = "" };
+        public readonly static FileData LinkToBackFileData = new() { Name = "..."};
     }
 }
